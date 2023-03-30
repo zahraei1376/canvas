f54681017dd6545a02cef8c48ae26a108ffb3d8c
zahra <zahra0a1418@gmail.com>
2023-03-30 16:16:14 +0430

    add animate act

--- script.js
+++ script.js
@@ -23,9 +23,7 @@ image1.addEventListener("load", () => {
     // ctx.putImageData(scannedImage, 0, 0);
 });
 ////////////////////////////////////
-
-
-function convertoGray() {
+const convertoGray = () => {
     ctx.drawImage(image1, 0, 0, canvas.width, canvas.height);
     const scannedImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
     console.log(scannedImage);
@@ -39,4 +37,59 @@ function convertoGray() {
     }
     scannedImage.data.scannedData;
     ctx.putImageData(scannedImage, 0, 0);
+}
+
+
+const convertoAnimate = () => {
+    ctx.drawImage(image1, 0, 0, canvas.width, canvas.height);
+
+    let particlesArray = [];
+    const numberOfParticles = 5000;
+
+    class Particle {
+        constructor() {
+            this.x = Math.random() * canvas.width;
+            this.y = 0;
+            this.speed = 0;
+            this.velocity = Math.random() * 3.5;
+            this.size = Math.random() * 1.5 + 1;
+        }
+
+        update() {
+            this.y += this.velocity;
+            if (this.y >= canvas.height) {
+                this.y = 0;
+                this.x = Math.random() * canvas.width;
+            }
+        }
+
+        draw() {
+            ctx.beginPath();
+            ctx.fillStyle = "white";
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+            ctx.fill();
+        }
+    }
+
+    function init() {
+        for (let i = 0; i < numberOfParticles; i++) {
+            particlesArray.push(new Particle)
+        }
+    }
+
+    init();
+
+    function animate() {
+        ctx.drawImage(image1, 0, 0, canvas.width, canvas.height);
+        ctx.globalAlpha = 0.05;
+        ctx.fillStyle = "rgb(0, 0, 0)";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        for (let i = 0; i < particlesArray.length; i++) {
+            particlesArray[i].update();
+            particlesArray[i].draw();
+        }
+        requestAnimationFrame(animate);
+    }
+
+    animate();
 }
