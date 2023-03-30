c7961a34ff726ea7e03307f0fd29c7e98e4496ae
zahra <zahra0a1418@gmail.com>
2023-03-30 16:12:31 +0430

    change color after click button

--- script.js
+++ script.js
@@ -9,6 +9,24 @@ image1.src = "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAQDAwQDAwQ
 
 image1.addEventListener("load", () => {
     ctx.drawImage(image1, 0, 0, canvas.width, canvas.height);
+    // const scannedImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    // console.log(scannedImage);
+    // const scannedData = scannedImage.data;
+    // for (let i = 0; i < scannedData.length; i += 4) {
+    //     const total = scannedData[i] + scannedData[i + 1] + scannedData[i + 2];
+    //     const averageColorValue = total / 3;
+    //     scannedData[i] = averageColorValue;
+    //     scannedData[i + 1] = averageColorValue;
+    //     scannedData[i + 2] = averageColorValue;
+    // }
+    // scannedImage.data.scannedData;
+    // ctx.putImageData(scannedImage, 0, 0);
+});
+////////////////////////////////////
+
+
+function convertoGray() {
+    ctx.drawImage(image1, 0, 0, canvas.width, canvas.height);
     const scannedImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
     console.log(scannedImage);
     const scannedData = scannedImage.data;
@@ -21,5 +39,4 @@ image1.addEventListener("load", () => {
     }
     scannedImage.data.scannedData;
     ctx.putImageData(scannedImage, 0, 0);
-});
-////////////////////////////////////
+}
